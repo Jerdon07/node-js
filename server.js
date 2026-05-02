d123e1766fb37b179e79a3946ee6dc82a767da51
Jerdon07 <jdlitaoen@gmail.com>
2026-05-02 15:54:26 +0800

    Req Object

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/html')
     // res.statusCode = 404
 
+    console.log(req.url)
+    console.log(req.method)
+
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end('<h1>Hello, World!</h1>')
 })
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port 8000 ${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
